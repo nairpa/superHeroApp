9e7905902ef4664248ee0a1f4277d988533e91ee
Nair <niir.hya@gmail.com>
2021-05-07 11:23:11 -0300

    Created: index of app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/styles/index.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import App from '../src/components/App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
